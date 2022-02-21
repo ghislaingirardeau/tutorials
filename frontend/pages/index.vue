cfa9f3d91feba721ff897893ecdc2bdf50ade5f4
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-02-21 15:45:04 +0700

    debug i18n input

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,13 +2,13 @@
   <v-row justify="center" align="center">
     <v-col cols="6">
       <h1>
-          {{$t('index.h1')}} {{browserLanguage}}
+          {{$t('index.h1')}}
       </h1>
     </v-col>
     <v-col cols="3">
       <v-text-field
-        :name="$t('Search')"
-        :label="$t('Search')"
+        :name="$t('index.search')"
+        :label="$t('index.search')"
         append-icon="mdi-map-marker"
       ></v-text-field>
     </v-col>
@@ -18,7 +18,7 @@
         :items="categoryItems"
         append-icon="mdi-map"
         menu-props="auto"
-        :label="$t('category')"
+        :label="$t('index.category')"
         single-line
       ></v-select>
     </v-col>
